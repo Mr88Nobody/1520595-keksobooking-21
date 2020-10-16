b9a059987340801fe953a784afd299391a4a57a9
Mr.Nobody <agwebdes@yandex.ru>
2020-10-16 17:14:00 +0300

    Переделал валидацию

--- js/main.js
+++ js/main.js
@@ -2,12 +2,14 @@
 
 const QUANTITY_ADVERTISEMENTS = 8;
 const FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
-const CHECKIN_TIMES = [`12:00`, `13:00`, `14:00`];
-const CHECKOUT_TIMES = [`12:00`, `13:00`, `14:00`];
+const CHECK_TIMES = [`12:00`, `13:00`, `14:00`];
 const TYPES = [`palace`, `flat`, `house`, `bungalow`];
 const PHOTOS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
 const X_COORDS = Math.round(65 / 2);
 const Y_COORDS = Math.round(65 / 2 + 18);
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+const MAX_PRICE = 1000000;
 
 const getRandomNumber = (min, max) => {
   min = Math.ceil(min);
@@ -38,8 +40,8 @@ const generateАdvertisement = () => {
       type: generateRandomItem(TYPES),
       rooms: getRandomNumber(1, 3),
       guests: getRandomNumber(1, 3),
-      checkin: generateRandomItem(CHECKIN_TIMES),
-      checkout: generateRandomItem(CHECKOUT_TIMES),
+      checkin: generateRandomItem(CHECK_TIMES),
+      checkout: generateRandomItem(CHECK_TIMES),
       features: generateRandomItem(FEATURES),
       description: `строка с описанием`,
       photos: generateRandomItem(PHOTOS)
@@ -84,16 +86,28 @@ const renderPins = () => {
   mapPins.append(fragment);
 };
 
-const mapPinMain = document.querySelector(`.map__pin--main`);
-const map = document.querySelector(`.map`);
-const adForm = document.querySelector(`.ad-form`);
-const fieldsets = document.querySelectorAll(`fieldset`);
-const mapFilters = document.querySelector(`.map__filters`).children;
+const activetePage = () => {
+  const mapPinMain = document.querySelector(`.map__pin--main`);
+  const map = document.querySelector(`.map`);
+  const mapFilters = document.querySelector(`.map__filters`).children;
+  const fieldsets = document.querySelectorAll(`fieldset`);
+  const adForm = document.querySelector(`.ad-form`);
 
-const activetedHandler = () => {
+  const makeActive = () => {
+    map.classList.remove(`map--faded`);
+    adForm.classList.remove(`ad-form--disabled`);
+    renderPins();
+    for (const fieldset of fieldsets) {
+      fieldset.removeAttribute(`disabled`, `disabled`);
+    }
+    for (const elementMapFilters of mapFilters) {
+      elementMapFilters.removeAttribute(`disabled`, `disabled`);
+    }
+  };
+
   if (adForm.classList.contains(`ad-form--disabled`)) {
-    for (const elem of mapFilters) {
-      elem.setAttribute(`disabled`, `disabled`);
+    for (const elementMapFilters of mapFilters) {
+      elementMapFilters.setAttribute(`disabled`, `disabled`);
     }
 
     for (const fieldset of fieldsets) {
@@ -101,48 +115,22 @@ const activetedHandler = () => {
     }
 
     mapPinMain.addEventListener(`mousedown`, (evtMousePressed) => {
-      evtMousePressed.preventDefault();
       if (evtMousePressed.button === 0) {
-        map.classList.remove(`map--faded`);
-        adForm.classList.remove(`ad-form--disabled`);
-        renderPins();
-        for (const fieldset of fieldsets) {
-          fieldset.removeAttribute(`disabled`, `disabled`);
-        }
-        for (const elem of mapFilters) {
-          elem.removeAttribute(`disabled`, `disabled`);
-        }
+        evtMousePressed.preventDefault();
+        makeActive();
       }
     });
 
     mapPinMain.addEventListener(`keydown`, (evtKeyPressed) => {
       if (evtKeyPressed.key === `Enter`) {
         evtKeyPressed.preventDefault();
-        map.classList.remove(`map--faded`);
-        adForm.classList.remove(`ad-form--disabled`);
-        renderPins();
-        for (const fieldset of fieldsets) {
-          fieldset.removeAttribute(`disabled`, `disabled`);
-        }
-        for (const elem of mapFilters) {
-          elem.removeAttribute(`disabled`, `disabled`);
-        }
+        makeActive();
       }
     });
   }
 };
 
-activetedHandler();
-
 const titleAdvertisement = document.querySelector(`#title`);
-const priceRent = document.querySelector(`#price`);
-const typeRent = document.querySelector(`#type`);
-
-const MIN_TITLE_LENGTH = 30;
-const MAX_TITLE_LENGTH = 100;
-// const MIN_PRICE = 0;
-const MAX_PRICE = 1000000;
-
 titleAdvertisement.addEventListener(`input`, () => {
   const valueLength = titleAdvertisement.value.length;
 
@@ -157,98 +145,114 @@ titleAdvertisement.addEventListener(`input`, () => {
   titleAdvertisement.reportValidity();
 });
 
-const valuePrice = priceRent.value;
+const getPrice = () => {
+  const priceRent = document.querySelector(`#price`);
+  const typeRent = document.querySelector(`#type`);
 
-typeRent.addEventListener(`change`, () => {
-  if (typeRent.value === `bungalow`) {
-    priceRent.value = 0;
-  }
-  if (typeRent.value === `flat`) {
-    priceRent.value = 1000;
-    priceRent.min = 1000;
-  }
-  if (typeRent.value === `house`) {
-    priceRent.value = 5000;
-    priceRent.min = 5000;
-  }
-  if (typeRent.value === `palace`) {
-    priceRent.value = 10000;
-    priceRent.min = 10000;
-  }
-});
+  typeRent.addEventListener(`change`, () => {
+    switch (typeRent.value) {
+      case `bungalow`:
+        priceRent.value = 0;
+        priceRent.setCustomValidity(`Бесплатно`);
+        priceRent.reportValidity();
+        break;
+      case `flat`:
+        priceRent.value = 1000;
+        priceRent.min = 1000;
+        priceRent.setCustomValidity(`Минимальная цена за ночь одна тысяча рублей`);
+        priceRent.reportValidity();
+        break;
+      case `house`:
+        priceRent.value = 5000;
+        priceRent.min = 5000;
+        priceRent.setCustomValidity(`Минимальная цена за ночь пять тысяч рублей`);
+        priceRent.reportValidity();
+        break;
+      case `palace`:
+        priceRent.value = 10000;
+        priceRent.min = 10000;
+        priceRent.setCustomValidity(`Минимальная цена за ночь десять тысяч рублей`);
+        priceRent.reportValidity();
+        break;
+    }
+  });
 
-priceRent.addEventListener(`input`, () => {
-  if (valuePrice > MAX_PRICE) {
-    priceRent.setCustomValidity(`Максимальная цена за ночь один миллион рублей`);
-  } else {
-    priceRent.setCustomValidity(``);
-  }
+  priceRent.addEventListener(`input`, () => {
+    const valuePrice = priceRent.value;
+    if (valuePrice > MAX_PRICE) {
+      priceRent.setCustomValidity(`Максимальная цена за ночь один миллион рублей`);
+    } else {
+      priceRent.setCustomValidity(``);
+    }
 
-  priceRent.reportValidity();
-});
+    priceRent.reportValidity();
+  });
+};
 
-const timein = document.querySelector(`#timein`);
-const timeout = document.querySelector(`#timeout`);
+const checkTime = () => {
+  const timein = document.querySelector(`#timein`);
+  const timeout = document.querySelector(`#timeout`);
 
-timein.addEventListener(`change`, () => {
-  if (timein.value === `12:00`) {
-    timeout.value = `12:00`;
-  }
-  if (timein.value === `13:00`) {
-    timeout.value = `13:00`;
-  }
-  if (timein.value === `14:00`) {
-    timeout.value = `14:00`;
-  }
-});
+  const getCheckTime = (inHour, outHour) => {
+    for (let i = 0; i < CHECK_TIMES.length; i++) {
+      const checkHour = CHECK_TIMES[i];
+      if (inHour.value === checkHour) {
+        outHour.value = checkHour;
+      }
+    }
+  };
 
-timeout.addEventListener(`change`, () => {
-  if (timeout.value === `12:00`) {
-    timein.value = `12:00`;
-  }
-  if (timeout.value === `13:00`) {
-    timein.value = `13:00`;
-  }
-  if (timeout.value === `14:00`) {
-    timein.value = `14:00`;
-  }
-});
+  timein.addEventListener(`change`, () => {
+    getCheckTime(timein, timeout);
+  });
 
-const rooms = document.querySelector(`#room_number`);
-const capacity = document.querySelector(`#capacity`);
+  timeout.addEventListener(`change`, () => {
+    getCheckTime(timeout, timein);
+  });
+};
 
-capacity.addEventListener(`change`, () => {
-  if (capacity.value === `0`) {
-    rooms.value = `100`;
-  }
-  if (capacity.value === `1`) {
-    rooms.value = `1`;
-    rooms.value = `2`;
-    rooms.value = `3`;
-  }
-  if (capacity.value === `2`) {
-    rooms.value = `2`;
-    rooms.value = `3`;
-  }
-  if (capacity.value === `3`) {
-    rooms.value = `3`;
-  }
-});
+const getCapacity = () => {
+  const rooms = document.querySelector(`#room_number`);
+  const capacity = document.querySelector(`#capacity`);
 
-rooms.addEventListener(`change`, () => {
-  if (rooms.value === `100`) {
-    capacity.value = `0`;
-  }
-  if (rooms.value === `1`) {
-    capacity.value = `1`;
-  }
-  if (rooms.value === `2`) {
-    capacity.value = `2`;
-    capacity.value = `1`;
-  }
-  if (rooms.value === `3`) {
-    capacity.value = `3`;
-    capacity.value = `2`;
-    capacity.value = `1`;
-  }
-});
+  capacity.addEventListener(`change`, () => {
+    switch (capacity.value) {
+      case `0`:
+        capacity.setCustomValidity(`100 комнат`);
+        break;
+      case `1`:
+        capacity.setCustomValidity(`Можете выбрать 1, 2, 3 комнаты`);
+        break;
+      case `2`:
+        capacity.setCustomValidity(`Можете выбрать 2 или 3 комнаты`);
+        break;
+      case `3`:
+        capacity.setCustomValidity(`Можете выбрать только 3 комнаты`);
+        break;
+    }
+    capacity.reportValidity();
+  });
+
+  rooms.addEventListener(`change`, () => {
+    switch (rooms.value) {
+      case `100`:
+        rooms.setCustomValidity(`Не для гостей`);
+        break;
+      case `1`:
+        rooms.setCustomValidity(`Только для одного человека`);
+        break;
+      case `2`:
+        rooms.setCustomValidity(`Для двоих или одного человек`);
+        break;
+      case `3`:
+        rooms.setCustomValidity(`Для троих, двоих или одного человека`);
+        break;
+    }
+    rooms.reportValidity();
+  });
+};
+
+activetePage();
+getPrice();
+checkTime();
+getCapacity();
